3a5c6e80e65b15938e300e5bd8d9dbbd8b060755
Joshua Thompson <121765426+joshuareecet@users.noreply.github.com>
2026-05-27 21:29:18 +0100

    Add: two-integer-sum - submission-8

---
+++ Data Structures & Algorithms/two-integer-sum/submission-8.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        std::unordered_map<int,int> mapper;
+        mapper.reserve(nums.size());
+        int desired_num {};
+        for (int i {}; i < nums.size(); ++i){
+            desired_num = target - nums[i];
+            if (mapper.find(desired_num) != mapper.end()){
+                return {mapper[desired_num], i};
+            }
+            else{
+                mapper[nums[i]] = i;
+            }
+        }
+        throw std::invalid_argument("no pair found");
+    }
+};
